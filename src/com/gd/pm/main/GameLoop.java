792a3090615688ecafa7e8b5b45ade8c3d656f83
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-10 01:00:51 +0800

    Integrate views in GameLoop

--- src/com/gd/pm/main/GameLoop.java
+++ src/com/gd/pm/main/GameLoop.java
@@ -1,17 +1,20 @@
 package com.gd.pm.main;
 
 import com.gd.pm.ui.GameWindow;
+import com.gd.pm.view.GameViewManager;
 
 import java.awt.Color;
 import java.awt.Graphics;
 
 public final class GameLoop {
-    private GameWindow gameWindow;
     private final int fps;
+    private GameWindow gameWindow;
+    private GameViewManager gameViewManager;
 
-    public GameLoop(int fps, GameWindow gameWindow) {
+    public GameLoop(int fps, GameWindow gameWindow, GameViewManager gameViewManager) {
         this.fps = fps;
         this.gameWindow = gameWindow;
+        this.gameViewManager = gameViewManager;
         gameWindow.setGameLoop(this);
     }
 
@@ -24,12 +27,14 @@ public final class GameLoop {
     }
 
     public void update() {
+        gameViewManager.update();
         System.out.println("Updating...");
     }
 
     public void render(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, gameWindow.getWidth(), gameWindow.getHeight());
+        gameViewManager.render(g);
         System.out.println("Rendering...");
     }
 }
