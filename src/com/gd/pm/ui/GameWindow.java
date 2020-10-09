5414995ba6db849540c475d6ebacea99994ca1ec
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-09 23:57:30 +0800

    Create a GameWindow that will wrap a JFrame to have a custom game window with restricted behavior

---
+++ src/com/gd/pm/ui/GameWindow.java
@@ -0,0 +1,40 @@
+package com.gd.pm.ui;
+
+import com.gd.pm.key.GameKeyManager;
+import com.gd.pm.key.GameKeyObserver;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public final class GameWindow {
+    private JFrame jFrame;
+    private GamePanel gamePanel;
+    private GameKeyManager gameKeyManager;
+
+    public GameWindow(String title, Dimension dimension) {
+        jFrame = new JFrame(title);
+        gamePanel = new GamePanel(dimension);
+        gameKeyManager = new GameKeyManager();
+
+        setup();
+    }
+
+    private void setup() {
+        jFrame.add(gamePanel);
+        jFrame.addKeyListener(gameKeyManager);
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setResizable(false);
+        jFrame.setLocationRelativeTo(null);
+    }
+
+    public void show() {
+        jFrame.setVisible(true);
+    }
+
+    public void addGameKeyObserver(GameKeyObserver gameKeyObserver) {
+        gameKeyManager.addGameKeyObserver(gameKeyObserver);
+    }
+}
