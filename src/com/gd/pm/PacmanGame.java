f0b94204bbe6cbf2cae5f5b4d3b5153352486597
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-10 00:27:16 +0800

    Create an instance of GameLoop

--- src/com/gd/pm/PacmanGame.java
+++ src/com/gd/pm/PacmanGame.java
@@ -2,19 +2,22 @@ package com.gd.pm;
 
 import com.gd.pm.key.GameKeyObserver;
 import com.gd.pm.key.observer.StartViewKeyObserver;
+import com.gd.pm.main.GameLoop;
 import com.gd.pm.ui.GameWindow;
 
 import java.awt.Dimension;
 
 public class PacmanGame {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GameWindow gameWindow = new GameWindow("Pacman", new Dimension(1000, 600));
-        gameWindow.show();
 
         GameKeyObserver startViewKeyObserver = new StartViewKeyObserver();
         startViewKeyObserver.setEnabled(true);
-
         gameWindow.addGameKeyObserver(startViewKeyObserver);
+
+        GameLoop gameLoop = new GameLoop(100, gameWindow);
+        gameWindow.show();
+        gameLoop.start();
     }
 }
