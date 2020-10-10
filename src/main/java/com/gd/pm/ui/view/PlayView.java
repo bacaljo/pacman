2a4ed1148bac02b1f25371025bf093f4c3da1022
John Emmanuel Bacalla <johnemmanuel.bacalla@gmail.com>
2020-10-10 22:20:37 +0800

    Instantiate a pacman object in PlayView and add movement controls

--- src/main/java/com/gd/pm/ui/view/PlayView.java
+++ src/main/java/com/gd/pm/ui/view/PlayView.java
@@ -1,5 +1,8 @@
 package com.gd.pm.ui.view;
 
+import com.gd.pm.gameplay.GameObjectManager;
+import com.gd.pm.gameplay.object.Pacman;
+import com.gd.pm.gameplay.property.Location;
 import com.gd.pm.key.GameKeyAction;
 import com.gd.pm.ui.GameView;
 import com.gd.pm.ui.graphic.GameGraphics;
@@ -7,15 +10,29 @@ import com.gd.pm.ui.property.HorizontalPosition;
 import com.gd.pm.ui.property.Position;
 import com.gd.pm.ui.property.VerticalPosition;
 
+import static com.gd.pm.gameplay.property.Direction.DOWN;
+import static com.gd.pm.gameplay.property.Direction.LEFT;
+import static com.gd.pm.gameplay.property.Direction.RIGHT;
+import static com.gd.pm.gameplay.property.Direction.UP;
 import static java.awt.Color.WHITE;
+import static java.awt.event.KeyEvent.VK_A;
 import static java.awt.event.KeyEvent.VK_BACK_SPACE;
+import static java.awt.event.KeyEvent.VK_D;
+import static java.awt.event.KeyEvent.VK_S;
 import static java.awt.event.KeyEvent.VK_SPACE;
+import static java.awt.event.KeyEvent.VK_W;
 
 public final class PlayView extends GameView {
     private boolean isPaused;
+    private GameObjectManager gameObjectManager;
+    private Pacman pacman;
 
     public PlayView() {
         super("play");
+        gameObjectManager = new GameObjectManager();
+
+        pacman = new Pacman(new Location(10, 10), UP);
+        gameObjectManager.addGameObject(pacman);
     }
 
     @Override
@@ -23,10 +40,14 @@ public final class PlayView extends GameView {
         if (isPaused) {
             return;
         }
+
+        gameObjectManager.update();
     }
 
     @Override
     public void render(GameGraphics gameGraphics) {
+        gameObjectManager.render(gameGraphics);
+
         gameGraphics.adjustFontFamily("AtlandSketches BB");
         gameGraphics.adjustFontSize(30);
 
@@ -66,12 +87,59 @@ public final class PlayView extends GameView {
 
             }
         });
-
         put(VK_SPACE, new GameKeyAction() {
 
             @Override
             public void press() {
                 isPaused = !isPaused;
+            }
+
+            @Override
+            public void release(double pressDuration) {
+
+            }
+        });
+        put(VK_W, new GameKeyAction() {
+
+            @Override
+            public void press() {
+                pacman.move(UP);
+            }
+
+            @Override
+            public void release(double pressDuration) {
+
+            }
+        });
+        put(VK_S, new GameKeyAction() {
+
+            @Override
+            public void press() {
+                pacman.move(DOWN);
+            }
+
+            @Override
+            public void release(double pressDuration) {
+
+            }
+        });
+        put(VK_A, new GameKeyAction() {
+
+            @Override
+            public void press() {
+                pacman.move(LEFT);
+            }
+
+            @Override
+            public void release(double pressDuration) {
+
+            }
+        });
+        put(VK_D, new GameKeyAction() {
+
+            @Override
+            public void press() {
+                pacman.move(RIGHT);
             }
 
             @Override
