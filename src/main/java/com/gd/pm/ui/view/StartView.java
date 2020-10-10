60b89a7d4e072375df85fb8dd9756531a5d83327
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-10 16:02:11 +0800

    Utilize GameGraphics in StartView and display the appropriate start view content

--- src/main/java/com/gd/pm/ui/view/StartView.java
+++ src/main/java/com/gd/pm/ui/view/StartView.java
@@ -1,10 +1,13 @@
 package com.gd.pm.ui.view;
 
 import com.gd.pm.key.observer.StartViewKeyObserver;
-import com.gd.pm.ui.GameDimension;
 import com.gd.pm.ui.GameView;
+import com.gd.pm.ui.graphic.GameGraphics;
+import com.gd.pm.ui.property.HorizontalPosition;
+import com.gd.pm.ui.property.Position;
+import com.gd.pm.ui.property.VerticalPosition;
 
-import java.awt.Graphics;
+import static java.awt.Color.WHITE;
 
 public final class StartView extends GameView {
     public StartView() {
@@ -17,7 +20,17 @@ public final class StartView extends GameView {
     }
 
     @Override
-    public void render(Graphics g, GameDimension gameDimension) {
+    public void render(GameGraphics gameGraphics) {
+        gameGraphics.adjustFontFamily("Verdana");
 
+        gameGraphics.adjustFontSize(144);
+        gameGraphics.drawString("Pacman",
+                new Position().setPositions(HorizontalPosition.MIDDLE, VerticalPosition.MIDDLE),
+                WHITE);
+
+        gameGraphics.adjustFontSize(24);
+        gameGraphics.drawString("Press space to play",
+                new Position().setPositions(HorizontalPosition.MIDDLE, VerticalPosition.BOTTOM).offsetYBy(-20),
+                WHITE);
     }
 }
