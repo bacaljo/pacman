544ae6910a71bbdcf0dc9f059a6877f6f69e881e
John Emmanuel Bacalla <johnemmanuel.bacalla@gmail.com>
2020-10-11 23:47:29 +0800

    Create a sample class that will inherit GameMap

---
+++ src/main/java/com/gd/pm/gameplay/map/PlainMap.java
@@ -0,0 +1,9 @@
+package com.gd.pm.gameplay.map;
+
+import com.gd.pm.gameplay.GameMap;
+
+public class PlainMap extends GameMap {
+    public PlainMap(int gridSize) {
+        super(10, 10, gridSize);
+    }
+}
